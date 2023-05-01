b2724290d1021c35874d7e45523fb9a861d785d2
Jeff Thomson <jeff.thomson@fingent.com>
2023-05-01 20:50:59 +0530

    fupdated training data to have {rows} rows and trained new model at {now}

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import joblib
 import datetime
+import os
 
 app = Flask(__name__)
 
@@ -12,16 +13,25 @@ def hello():
 
 @app.route('/train')
 def get_data_and_train():
+    data_path='./result/model_with_data'
     data = pd.read_csv('house_price.csv')
-    data=data[:20].copy()
+    data=data[:25].copy()
     data.to_csv('./result/model_with_data/training_data.csv')
     X = data[['area', 'bedrooms', 'bathrooms', 'stories']]
     y = data['price']   
     model = LinearRegression()
+    rows=len(X)
     model.fit(X, y)
     joblib.dump(model, './result/model_with_data/house_price_prediction_model.pkl')
-
-
+    now = datetime.datetime.now()
+    filename = 'result/model_with_data/model_details.txt'
+    with open(filename, 'a') as f:
+        f.write(f'model trained at {now} with {rows} rows of data' + '\n')
+    os.system(f'dvc add {data_path}')
+    os.system('dvc push')
+    os.system('git add .')
+    os.system('git commit -m f"updated training data to have {rows} rows and trained new model at {now}"')
+    os.system('git push origin main')
     return 'Done'
 
 
