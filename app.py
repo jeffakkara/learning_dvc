c99da6d2f382f164980c6d7a6fd7f72e00f3e5c0
Jeff Thomson <jeff.thomson@fingent.com>
2023-05-01 16:43:06 +0530

    trained using 20 rows and updated model

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import joblib
+import datetime
 
 app = Flask(__name__)
 
@@ -12,13 +13,14 @@ def hello():
 @app.route('/train')
 def get_data_and_train():
     data = pd.read_csv('house_price.csv')
-    data=data[:10].copy()
+    data=data[:20].copy()
     data.to_csv('./result/model_with_data/training_data.csv')
     X = data[['area', 'bedrooms', 'bathrooms', 'stories']]
     y = data['price']   
     model = LinearRegression()
     model.fit(X, y)
     joblib.dump(model, './result/model_with_data/house_price_prediction_model.pkl')
+
 
     return 'Done'
 
