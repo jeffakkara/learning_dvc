173e390bb76586e9a18c338a554fd4cc97e057bf
Jeff Thomson <jeff.thomson@fingent.com>
2023-05-01 14:28:21 +0530

    added dvc and csv file for training simulation

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/train')
+def get_data_and_train():
+    data = pd.read_csv('house_price.csv')
+    data=data[:5].copy()
+    data.to_csv('./data/training_data.csv')
+    X = data[['area', 'bedrooms', 'bathrooms', 'stories']]
+    y = data['price']   
+    model = LinearRegression()
+    model.fit(X, y)
+    joblib.dump(model, './data/house_price_prediction_model.pkl')
+
+    return 'Done'
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
